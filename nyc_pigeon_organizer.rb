aadbb385e6e854d0cc1f5e10a261c4152d98a4cf
Xinran Che <gracexinran@gmail.com>
2019-05-23 00:10:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  list = {}
+  data.each do |k, v|
+    v.each do |key,value|
+      value.each do |name|
+        if !list[name]
+          list[name] = [value.to_s]
+        else
+          list[name] << [value.to_s]
+        end
+      end
+    end
+  end
+  list
+        
 end
